faaf8e2b08fe6c423983cf0c202a7b66bc8c3f9d
jcrossley <jim@crossleys.org>
2010-09-06 01:58:14 -0400

    Grabs the latest ADP list from fantasyfootballcalculator.com and integrates the tiers.

---
+++ players.rb
@@ -0,0 +1,23 @@
+ADP_URL = 'http://fantasyfootballcalculator.com/adp_xml.php?teams=12'
+
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'ostruct'
+require 'tiers'
+
+doc = Nokogiri::XML(open(ADP_URL))
+
+players = (doc/'player').map do |player|
+  adp, name, pos, team = (player%'adp').text, (player%'name').text, (player%'pos').text, (player%'team').text
+  OpenStruct.new :adp => adp, :name => name, :pos => pos, :team => team, :tier => TIERS[name]
+end
+
+if ARGV[0] == '-t'
+  players = players.reject {|p| p.tier.nil?}.sort_by { |p| format("%3s %2s %5s", p.pos, p.tier, p.adp) }
+end
+
+players.each do |p|
+  puts format("%5s  %-25s %3s  %3s  %2s", p.adp, p.name, p.team, p.pos, p.tier)
+end
+
